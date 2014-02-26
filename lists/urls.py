8679f288f11b2a86a017bf3fef7e93d27393b514
continueing <continueing@gmail.com>
2014-02-26 17:18:05 +0900

    Refactor away some duplication in urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
+    url(r'^(\d+)/$', 'lists.views.view_list',name='view_list'),
+    url(r'^(\d+)/new_item$','lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_lists',name='new_lists')
+    ,
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
